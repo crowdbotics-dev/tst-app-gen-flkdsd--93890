8f34bea3a5538ffc8f2d20c02329d6fde237bbb9
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-22 16:56:13 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Hello(models.Model):
     'Generated Model'
     daaddsads = models.BigIntegerField()
+class Hi(models.Model):
+    'Generated Model'
+    dadasdds = models.BigIntegerField()
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserUpdateView, user_redirect_view, user_detail_view, UserDetailView, user_update_view
+from .views import user_redirect_view, UserUpdateView, user_detail_view, user_update_view, UserDetailView
 
 app_name = "users"
 urlpatterns = [
